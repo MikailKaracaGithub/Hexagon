92f09149dabf42f3eb743ad7bf482ee2d7a6597c
Xavaltir <tombis1999@hotmail.com>
2021-01-06 00:45:30 +0100

    Coordinates generation
    Axial coordinates system point generation made and they are stores in a dictionary

---
+++ Assets/Scripts/BoardSystem/Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BoardSystem
+{ 
+public class Board : MonoBehaviour
+ {
+        private Dictionary<Position, Tile> _tiles = new Dictionary<Position, Tile>();
+        public IList<Tile> Tiles => _tiles.Values.ToList();
+        private void Start()
+        {
+            var radius = 5;
+            for (var i = 0; i < radius; i++)
+            {
+                for (var r = -i; r <= i; r++)
+                    for (var g = -i; g <= i; g++)
+                        for (var b = -i; b <= i; b++)
+                            if (Math.Abs(r) + Math.Abs(g) + Math.Abs(b) == i * 2 && r + g + b == 0)
+                            {
+                                Debug.Log(r + "," + g + "," + b);
+                                _tiles.Add(new Position { X = r, Y = g, Z = b }, new Tile(r, g, b));
+                                Debug.Log(Tiles.Count());
+                            }
+                Debug.Log("Coordinates have been generated");
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/BoardSystem/Tile.cs
@@ -0,0 +1,12 @@
+﻿namespace BoardSystem
+{
+    public class Tile
+    {
+        public Position Position { get; } // so i can access the tiles position
+
+        public Tile(int x, int y, int z)
+        {
+            Position = new Position { X = x, Y = y , Z = z};
+        }
+    }
+}
